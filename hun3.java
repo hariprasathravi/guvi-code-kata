9c6eee292d3ed1f9a3b829b6dd901d5a1c8f0b3b
hariprasathravi <hariprasath4441@gmail.com>
2016-10-13 10:33:54 +0530

    Create hun3.java

---
+++ hun3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class arrayequalindex
+ {
+  public static void main(String args[])
+   {
+    int arr[]={-1,0,1,3,5,7};
+    int index;
+    for(index=0;index<arr.length;index++)
+    if(index==arr[index])
+    System.out.println(+arr[index]+",");
+   }
+ }
